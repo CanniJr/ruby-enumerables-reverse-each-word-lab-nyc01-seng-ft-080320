deba3b90eb6416f04c1c9245ee6a286eb958e5a1
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 15:34:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
-def reverse_each_words(string)
-  string.split(" ").collect {|words|words.reverse}.join(" ")
+def reverse_each_word(sentence)
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
 end
